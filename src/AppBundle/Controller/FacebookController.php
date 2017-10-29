871edd7b50274e623ae5306faf1a409c96c4e7fa
Benas Rimsa <benas.rimsa@gmail.com>
2017-10-30 01:30:00 +0200

    Integrated Facebook login

---
+++ src/AppBundle/Controller/FacebookController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class FacebookController extends Controller
+{
+
+    /**
+ * @Route("/connect/facebook")
+ */
+    public function connectAction()
+    {
+        return $this->get('oauth2.registry')
+            ->getClient('facebook_main')
+            ->redirect();
+    }
+
+
+
+
+    /**
+     * @Route("/connect/facebook/check", name="connect_facebook_check")
+     */
+    public function connectCheckAction(Request $request)
+    {
+
+        $user = $this->get('security.token_storage')->getToken()->getUser();
+
+        return $this->render('AppBundle:Home:user.html.twig', array('user' => $user));
+    }
+
+}
