8c682320ad3c401cedb51974d6c377b13c7541e1
Benas Rimsa <benas.rimsa@gmail.com>
2017-11-13 20:56:42 +0200

    update deploy.rb

--- app/deploy.rb
+++ app/deploy.rb
@@ -12,5 +12,12 @@ set :linked_files, fetch(:linked_files, []).push('app/config/parameters.yml')
 set :linked_dirs, fetch(:linked_dirs, []).push('var/logs')
 
 # Defaults to :db role
-set :migration_role, :app
+set :migration_role, :db
+
+# Defaults to the primary :db server
+set :migration_servers, -> { primary(fetch(:migration_role)) }
+
+# Defaults to false
+# Skip migration if files in db/migrate were not modified
+set :conditionally_migrate, true
 
