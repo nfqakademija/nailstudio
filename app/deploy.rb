8e6cc11dd9dc8e26e8913acaa12cb0afa066f3e1
Benas Rimsa <benas.rimsa@gmail.com>
2017-11-14 22:02:35 +0200

    dotn use deploy migration

--- app/deploy.rb
+++ app/deploy.rb
@@ -10,14 +10,3 @@ set :exclude, ['.git', 'node_modules', 'web/app_dev.php']
 
 set :linked_files, fetch(:linked_files, []).push('app/config/parameters.yml')
 set :linked_dirs, fetch(:linked_dirs, []).push('var/logs')
-
-after 'deploy:finishing', 'mycompany:migrations:migrate'
-
-namespace :mycompany do
-  desc 'Executes a migration to the latest available version'
-  namespace :migrations do
-    task :migrate do
-      invoke 'symfony:console', 'doctrine:migrations:migrate', '--no-interaction'
-    end
-  end
-end
--- app/deploy/stage.rb
+++ app/deploy/stage.rb
@@ -1,3 +1,1 @@
 server 'deploy.nfqakademija.lt', user: 'nailstudio', roles: %w{web}
-
-after 'deploy:finishing', 'mycompany:migrations:migrate'
