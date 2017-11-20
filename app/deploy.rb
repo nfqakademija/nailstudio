43a383ec2f27a9d8e7f56e54decf364ac22e5469
Benas Rimsa <benas.rimsa@gmail.com>
2017-11-20 02:26:37 +0200

    updated deploy for db migrations

--- app/deploy.rb
+++ app/deploy.rb
@@ -16,11 +16,3 @@ before 'deploy:updated', 'symfony:doctrine:cache:clear_query'
 before 'deploy:updated', 'symfony:doctrine:cache:clear_result'
 after  'deploy:updated', 'symfony:doctrine:migrations'
 
-set :symfony_doctrine_cache_clear_metadata_flags, '--env=prod'
-set :symfony_doctrine_cache_clear_metadata_roles, :db
-set :symfony_doctrine_cache_clear_query_flags, '--env=prod'
-set :symfony_doctrine_cache_clear_query_roles, :db
-set :symfony_doctrine_cache_clear_result_flags, '--env=prod'
-set :symfony_doctrine_cache_clear_result_roles, :db
-set :symfony_doctrine_migrations_flags, '--no-interaction --allow-no-migration'
-set :symfony_doctrine_migrations_roles, :db
