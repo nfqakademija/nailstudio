64105cd79ef2b91e3717fc018aebe2e3dab80cfc
Benas Rimsa <benas.rimsa@gmail.com>
2017-12-07 16:09:04 +0200

    update navbar scroll

--- src/AppBundle/Resources/scripts/main.js
+++ src/AppBundle/Resources/scripts/main.js
@@ -1,272 +1,188 @@
+$(document).ready(function () {
 // Map
-function myMap() {
-    var mapCanvas = document.getElementById("map");
-    var myCenter = new google.maps.LatLng(55.9306458, 23.3162949);
-    var mapOptions = {
-        center: myCenter,
-        zoom: 16,
-        scrollwheel: false,
-        // navigationControl: false,
-        // mapTypeControl: false,
-        // scaleControl: false,
-        // How you would like to style the map.
-        // This is where you would paste any style found on Snazzy Maps.
-    //     styles: [{
-    //         "featureType": "administrative",
-    //         "elementType": "all",
-    //         "stylers": [{
-    //             "visibility": "on"
-    //         }, {
-    //             "lightness": 33
-    //         }]
-    //     }, {
-    //         "featureType": "administrative",
-    //         "elementType": "labels",
-    //         "stylers": [{
-    //             "saturation": "-100"
-    //         }]
-    //     }, {
-    //         "featureType": "administrative",
-    //         "elementType": "labels.text",
-    //         "stylers": [{
-    //             "gamma": "0.75"
-    //         }]
-    //     }, {
-    //         "featureType": "administrative.neighborhood",
-    //         "elementType": "labels.text.fill",
-    //         "stylers": [{
-    //             "lightness": "-37"
-    //         }]
-    //     }, {
-    //         "featureType": "landscape",
-    //         "elementType": "geometry",
-    //         "stylers": [{
-    //             "color": "#f9f9f9"
-    //         }]
-    //     }, {
-    //         "featureType": "landscape.man_made",
-    //         "elementType": "geometry",
-    //         "stylers": [{
-    //             "saturation": "-100"
-    //         }, {
-    //             "lightness": "40"
-    //         }, {
-    //             "visibility": "off"
-    //         }]
-    //     }, {
-    //         "featureType": "landscape.natural",
-    //         "elementType": "labels.text.fill",
-    //         "stylers": [{
-    //             "saturation": "-100"
-    //         }, {
-    //             "lightness": "-37"
-    //         }]
-    //     }, {
-    //         "featureType": "landscape.natural",
-    //         "elementType": "labels.text.stroke",
-    //         "stylers": [{
-    //             "saturation": "-100"
-    //         }, {
-    //             "lightness": "100"
-    //         }, {
-    //             "weight": "2"
-    //         }]
-    //     }, {
-    //         "featureType": "landscape.natural",
-    //         "elementType": "labels.icon",
-    //         "stylers": [{
-    //             "saturation": "-100"
-    //         }]
-    //     }, {
-    //         "featureType": "poi",
-    //         "elementType": "geometry",
-    //         "stylers": [{
-    //             "saturation": "-100"
-    //         }, {
-    //             "lightness": "80"
-    //         }]
-    //     }, {
-    //         "featureType": "poi",
-    //         "elementType": "labels",
-    //         "stylers": [{
-    //             "saturation": "-100"
-    //         }, {
-    //             "lightness": "0"
-    //         }]
-    //     }, {
-    //         "featureType": "poi.attraction",
-    //         "elementType": "geometry",
-    //         "stylers": [{
-    //             "lightness": "-4"
-    //         }, {
-    //             "saturation": "-100"
-    //         }]
-    //     }, {
-    //         "featureType": "poi.park",
-    //         "elementType": "geometry",
-    //         "stylers": [{
-    //             "color": "#c5dac6"
-    //         }, {
-    //             "visibility": "on"
-    //         }, {
-    //             "saturation": "-95"
-    //         }, {
-    //             "lightness": "62"
-    //         }]
-    //     }, {
-    //         "featureType": "poi.park",
-    //         "elementType": "labels",
-    //         "stylers": [{
-    //             "visibility": "on"
-    //         }, {
-    //             "lightness": 20
-    //         }]
-    //     }, {
-    //         "featureType": "road",
-    //         "elementType": "all",
-    //         "stylers": [{
-    //             "lightness": 20
-    //         }]
-    //     }, {
-    //         "featureType": "road",
-    //         "elementType": "labels",
-    //         "stylers": [{
-    //             "saturation": "-100"
-    //         }, {
-    //             "gamma": "1.00"
-    //         }]
-    //     }, {
-    //         "featureType": "road",
-    //         "elementType": "labels.text",
-    //         "stylers": [{
-    //             "gamma": "0.50"
-    //         }]
-    //     }, {
-    //         "featureType": "road",
-    //         "elementType": "labels.icon",
-    //         "stylers": [{
-    //             "saturation": "-100"
-    //         }, {
-    //             "gamma": "0.50"
-    //         }]
-    //     }, {
-    //         "featureType": "road.highway",
-    //         "elementType": "geometry",
-    //         "stylers": [{
-    //             "color": "#c5c6c6"
-    //         }, {
-    //             "saturation": "-100"
-    //         }]
-    //     }, {
-    //         "featureType": "road.highway",
-    //         "elementType": "geometry.stroke",
-    //         "stylers": [{
-    //             "lightness": "-13"
-    //         }]
-    //     }, {
-    //         "featureType": "road.highway",
-    //         "elementType": "labels.icon",
-    //         "stylers": [{
-    //             "lightness": "0"
-    //         }, {
-    //             "gamma": "1.09"
-    //         }]
-    //     }, {
-    //         "featureType": "road.arterial",
-    //         "elementType": "geometry",
-    //         "stylers": [{
-    //             "color": "#e4d7c6"
-    //         }, {
-    //             "saturation": "-100"
-    //         }, {
-    //             "lightness": "47"
-    //         }]
-    //     }, {
-    //         "featureType": "road.arterial",
-    //         "elementType": "geometry.stroke",
-    //         "stylers": [{
-    //             "lightness": "-12"
-    //         }]
-    //     }, {
-    //         "featureType": "road.arterial",
-    //         "elementType": "labels.icon",
-    //         "stylers": [{
-    //             "saturation": "-100"
-    //         }]
-    //     }, {
-    //         "featureType": "road.local",
-    //         "elementType": "geometry",
-    //         "stylers": [{
-    //             "color": "#fbfaf7"
-    //         }, {
-    //             "lightness": "77"
-    //         }]
-    //     }, {
-    //         "featureType": "road.local",
-    //         "elementType": "geometry.fill",
-    //         "stylers": [{
-    //             "lightness": "-5"
-    //         }, {
-    //             "saturation": "-100"
-    //         }]
-    //     }, {
-    //         "featureType": "road.local",
-    //         "elementType": "geometry.stroke",
-    //         "stylers": [{
-    //             "saturation": "-100"
-    //         }, {
-    //             "lightness": "-15"
-    //         }]
-    //     }, {
-    //         "featureType": "transit.station.airport",
-    //         "elementType": "geometry",
-    //         "stylers": [{
-    //             "lightness": "47"
-    //         }, {
-    //             "saturation": "-100"
-    //         }]
-    //     }, {
-    //         "featureType": "water",
-    //         "elementType": "all",
-    //         "stylers": [{
-    //             "visibility": "on"
-    //         }, {
-    //             "color": "#acbcc9"
-    //         }]
-    //     }, {
-    //         "featureType": "water",
-    //         "elementType": "geometry",
-    //         "stylers": [{
-    //             "saturation": "53"
-    //         }]
-    //     }, {
-    //         "featureType": "water",
-    //         "elementType": "labels.text.fill",
-    //         "stylers": [{
-    //             "lightness": "-42"
-    //         }, {
-    //             "saturation": "17"
-    //         }]
-    //     }, {
-    //         "featureType": "water",
-    //         "elementType": "labels.text.stroke",
-    //         "stylers": [{
-    //             "lightness": "61"
-    //         }]
-    //     }]
-    };
+    function myMap() {
+        var mapCanvas = document.getElementById("map");
+        var myCenter = new google.maps.LatLng(55.9306458, 23.3162949);
+        var mapOptions = {
+            center: myCenter,
+            zoom: 16,
+            scrollwheel: false,
+        };
 
 
-    var map = new google.maps.Map(mapCanvas,mapOptions);
-    var marker = new google.maps.Marker({
-        position: myCenter,
-        //animation: google.maps.Animation.BOUNCE
-    });
-    var infowindow = new google.maps.InfoWindow({
-        content: "Eglės Nagų Studija. Vilniaus g. 134 (2 aukštas) Šiauliai"
+        var map = new google.maps.Map(mapCanvas, mapOptions);
+        var marker = new google.maps.Marker({
+            position: myCenter,
+            //animation: google.maps.Animation.BOUNCE
+        });
+        var infowindow = new google.maps.InfoWindow({
+            content: "Eglės Nagų Studija. Vilniaus g. 134 (2 aukštas) Šiauliai"
+        });
+
+        infowindow.open(map, marker);
+        marker.setMap(map);
+    }
+
+
+// ---------------------------------------------- //
+// Preventing URL update on navigation link click
+// ---------------------------------------------- //
+$('.navbar-nav a, #scroll-down').bind('click', function (e) {
+    var anchor = $(this);
+    $('html, body').stop().animate({
+        scrollTop: $(anchor.attr('href')).offset().top
+    }, 1000);
+    e.preventDefault();
+});
+
+// ---------------------------------------------- //
+// Navbar Shrinking Behavior
+// ---------------------------------------------- //
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 20) {
+            $('nav.navbar').addClass('shrink');
+        } else {
+            $('nav.navbar').removeClass('shrink');
+        }
     });
 
-    infowindow.open(map,marker);
-    marker.setMap(map);
-}
+// ---------------------------------------------- //
+// Scroll to top button
+// ---------------------------------------------- //
+    $('#scrollTop').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1000);
+    });
 
+    $(window).scroll(function () {
+        if ($(window).scrollTop() >= 1500) {
+            $('#scrollTop').fadeIn();
+        } else {
+            $('#scrollTop').fadeOut();
+        }
+    });
+//
+// // Select all links with hashes
+// $('a[href*="#"]')
+// // Remove links that don't actually link to anything
+//     .not('[href="#"]')
+//     .not('[href="#0"]')
+//     .click(function(event) {
+//         // On-page links
+//         if (
+//             location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+//             &&
+//             location.hostname == this.hostname
+//         ) {
+//             // Figure out element to scroll to
+//             var target = $(this.hash);
+//             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+//             // Does a scroll target exist?
+//             if (target.length) {
+//                 // Only prevent default if animation is actually gonna happen
+//                 event.preventDefault();
+//                 $('html, body').animate({
+//                     scrollTop: target.offset().top
+//                 }, 1000, function() {
+//                     // Callback after animation
+//                     // Must change focus!
+//                     var $target = $(target);
+//                     $target.focus();
+//                     if ($target.is(":focus")) { // Checking if the target was focused
+//                         return false;
+//                     } else {
+//                         $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+//                         $target.focus(); // Set focus again
+//                     };
+//                 });
+//             }
+//         }
+//     });
+//
+// function Scroll() {
+//
+//     var navbar = document.getElementById('startchange');
+//     var ypos = window.pageYOffset;
+//     if (ypos > 500) {
+//         navbar.style.backgroundColor = "#31b0d5";
+//
+//     }
+//     else {
+//         navbar.style.backgroundColor = "darksalmon";
+//         // brand.style.color = "white";
+//     }
+// }
+
+// $(function() {
+//     //  jQueryUI 1.10 and HTML5 ready
+//     //      http://jqueryui.com/upgrade-guide/1.10/#removed-cookie-option
+//     //  Documentation
+//     //      http://api.jqueryui.com/tabs/#option-active
+//     //      http://api.jqueryui.com/tabs/#event-activate
+//     //      http://balaarjunan.wordpress.com/2010/11/10/html5-session-storage-key-things-to-consider/
+//     //
+//     //  Define friendly index name
+//     var index = 'key';
+//     //  Define friendly data store name
+//     var dataStore = window.sessionStorage;
+//     //  Start magic!
+//     try {
+//         // getter: Fetch previous value
+//         var oldIndex = dataStore.getItem(index);
+//     } catch(e) {
+//         // getter: Always default to first tab in error state
+//         var oldIndex = 0;
+//     }
+//     $('#tabs').tabs({
+//         // The zero-based index of the panel that is active (open)
+//         active : oldIndex,
+//         // Triggered after a tab has been activated
+//         activate : function( event, ui ){
+//             //  Get future value
+//             var newIndex = ui.newTab.parent().children().index(ui.newTab);
+//             //  Set future value
+//             dataStore.setItem( index, newIndex )
+//         }
+//     });
+// });
+
+//
+// $(document).ready(function () {
+//     $(document).on("scroll", onScroll);
+//
+//     //smoothscroll
+//     $('a[href^="#"]').on('click', function (e) {
+//         e.preventDefault();
+//         $(document).off("scroll");
+//
+//         $('a').each(function () {
+//             $(this).removeClass('active');
+//         })
+//         $(this).addClass('active');
+//
+//         var target = this.hash,
+//             menu = target;
+//         $target = $(target);
+//         $('html, body').stop().animate({
+//             'scrollTop': $target.offset().top+2
+//         }, 500, 'swing', function () {
+//             window.location.hash = target;
+//             $(document).on("scroll", onScroll);
+//         });
+//     });
+// });
+//
+// function onScroll(event){
+//     var scrollPos = $(document).scrollTop();
+//     $('#menu-center a').each(function () {
+//         var currLink = $(this);
+//         var refElement = $(currLink.attr("href"));
+//         if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+//             $('#menu-center ul li a').removeClass("active");
+//             currLink.addClass("active");
+//         }
+//         else{
+//             currLink.removeClass("active");
+//         }
+//     });
+// }
+});
