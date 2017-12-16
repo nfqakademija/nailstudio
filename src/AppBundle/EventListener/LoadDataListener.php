be1cdc344e1b435edbfc06b1a94acdb46be87038
Benas Rimsa <benas.rimsa@gmail.com>
2017-12-17 00:24:23 +0200

    fix for TRAVIS CI

--- src/AppBundle/EventListener/LoadDataListener.php
+++ src/AppBundle/EventListener/LoadDataListener.php
@@ -49,14 +49,11 @@ class LoadDataListener
         // You may want to add an Event into the Calendar view.
         /** @var Schedule $schedule */
 
-        foreach ($schedules as $schedule)
-        {
-            $calendarEvent->addEvent(
-                new Event(
+        foreach ($schedules as $schedule) {
+            $calendarEvent->addEvent(new Event(
                 $schedule->getTitle(),
                 $schedule->getStart(),
-                $schedule->getEnd())
-            );
+                $schedule->getEnd()));
         }
     }
 }
